9d39621c456f3cbfd8486494875934217c5a8b56
Cleverson Goulart <cleversonagoulart@gmail.com>
2021-05-25 20:51:28 -0300

    Realiza a operação escolhida pelo usuário com dois inteiros

--- calculadora.py
+++ calculadora.py
@@ -1,4 +1,10 @@
 a=int(input("Digite o primeiro inteiro: "))
 b=int(input("Digite o segundo inteiro: "))
-resultado=a+b
+operacao=input("Escolha a operação: (+) soma ou (-) subtração. Digite o símbolo da operação escolhida: ")
+if(operacao=='+'):
+    resultado=a+b
+elif(operacao=='-'):
+    resultado=a-b
+else:
+    print("Operação inválida")
 print(resultado) 
