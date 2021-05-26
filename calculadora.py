d834a1b402361a83bbad24594fc4c8d5a30768dd
Cleverson Goulart <cleversonagoulart@gmail.com>
2021-05-25 21:11:02 -0300

    Calculadora de dois números com as 4 operações basicas

--- calculadora.py
+++ calculadora.py
@@ -1,10 +1,14 @@
 a=int(input("Digite o primeiro inteiro: "))
 b=int(input("Digite o segundo inteiro: "))
-operacao=input("Escolha a operação: (+) soma ou (-) subtração. Digite o símbolo da operação escolhida: ")
+operacao=input("Escolha a operação:\n (+) soma\n (-) subtração\n (*) multiplicação\n (/) divisão\n Digite o símbolo da operação escolhida: ")
 if(operacao=='+'):
     resultado=a+b
 elif(operacao=='-'):
     resultado=a-b
+elif(operacao=='*'):
+    resultado=a*b
+elif(operacao=='/'):
+    resultado=a//b
 else:
     print("Operação inválida")
 print(resultado) 
