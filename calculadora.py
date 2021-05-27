9caa923271e91b71f71d095f60dae19ca7c3465e
Cleverson Goulart <cleversonagoulart@gmail.com>
2021-05-27 17:36:07 -0300

    Exponenciação adicionada à calculadora

--- calculadora.py
+++ calculadora.py
@@ -1,6 +1,6 @@
 a=int(input("Digite o primeiro inteiro: "))
 b=int(input("Digite o segundo inteiro: "))
-operacao=input("Escolha a operação:\n (+) soma\n (-) subtração\n (*) multiplicação\n (/) divisão\n Digite o símbolo da operação escolhida: ")
+operacao=input("Escolha a operação:\n (+) soma\n (-) subtração\n (*) multiplicação\n (/) divisão\n (**) exponenciação \n Digite o símbolo da operação escolhida:  ")
 if(operacao=='+'):
     resultado=a+b
 elif(operacao=='-'):
@@ -9,6 +9,8 @@ elif(operacao=='*'):
     resultado=a*b
 elif(operacao=='/'):
     resultado=a//b
+elif(operacao=='**'):
+    resultado=a**b
 else:
     print("Operação inválida")
 print(resultado) 
